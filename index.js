d23db9103e782e0bc2a4f648fbc5860f17c89111
MariKutubidze <82153875+Marikutubidze@users.noreply.github.com>
2022-06-03 21:21:03 +0400

    update index.js

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const icon = document.getElementById("icon");
 const icon1 = document.getElementById("a");
 const icon2 = document.getElementById("b");
 const icon3 = document.getElementById("c");
-const nav = document.getElementById('nav');
+const nav = document.getElementById("nav");
 const blue = document.getElementById("blue");
 
-icon.addEventListener('click', function() {
-  icon1.classList.toggle('a');
-  icon2.classList.toggle('c');
-  icon3.classList.toggle('b');
-  nav.classList.toggle('show');
-  blue.classList.toggle('slide');
+icon.addEventListener("click", function () {
+  icon1.classList.toggle("a");
+  icon2.classList.toggle("c");
+  icon3.classList.toggle("b");
+  nav.classList.toggle("show");
+  blue.classList.toggle("slide");
 });
+
+let accordion = document.getElementsByClassName("container");
+
+for (let i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+}
